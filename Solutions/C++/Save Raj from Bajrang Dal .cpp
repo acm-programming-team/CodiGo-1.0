239d8b48284af22b2f8c2f3fafeec5312cfade51
himanshu-j26 <46198757+himanshu-j26@users.noreply.github.com>
2019-02-15 00:34:30 +0530

    Update Save Raj from Bajrang Dal .cpp

--- Solutions/C++/Save Raj from Bajrang Dal .cpp
+++ Solutions/C++/Save Raj from Bajrang Dal .cpp
@@ -3,17 +3,17 @@ using namespace std;
 int main()
 {
     long long int n,i,sum=0;
-    cin>>n;
+    cin>>n;          // n is number of dishes in the Hotel // input taken from the user
     long long int a[n];
     for(i=0;i<n;i++)
     {
-        cin>>a[i];
+        cin>>a[i];  // input taken from user //dishes price
     }
-    sort(a,a+n);
+    sort(a,a+n);    //sort an array //according to prices
     for(i=0;i<n/2;i++)
     {
-        int s=a[i]+a[n-1-i];
-        sum+=(s*s);
+        int s=a[i]+a[n-1-i];    //add the i th and n-i-1 th value of sorted array
+        sum+=(s*s);             //sum according to the given equation
         
     }
     cout<<sum;
